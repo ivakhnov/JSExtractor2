97fdef63843fe28bf94c79c025ce59bb98ebc1d7
Evgeni Ivakhnov <ivachnov@hotmail.com>
2012-11-12 18:23:22 +0100

    Change express server configuration.

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
 });
 /////////////////////////////////////////////////////////////
