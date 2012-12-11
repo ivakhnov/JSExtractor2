ea30b188486b3987c36c5e780986c7d6dc4de8df
Evgeni Ivakhnov <ivachnov@hotmail.com>
2012-12-11 23:36:51 +0100

    Getting extracted scripts from DB and showing it on page.

--- routes/scripts.js
+++ routes/scripts.js
@@ -1,13 +1,19 @@
+/**
+ * Module dependencies
+ */
+var db = require('../lib/dbManager');
+var async = require('async');
+
+
 /*
  * Setup the routes and request handlers.
  */
 module.exports = function(app){
+	
+	app.get('/scripts', function(req, res){
+		getScripts(res);
+	});
 
-    app.get('/scripts', function(req, res){
-    	scripts(res);
-    });
-
-    //other routes..
 };
 
 
@@ -15,7 +21,29 @@ module.exports = function(app){
  * Controller functions.
  */
 
-
-var scripts = function(res){
-  res.render('scripts');
+var getScripts = function(res){
+		console.log('opgeslagen in sessie webpage: ' + res.locals.userUrl); 
+		db.getScripts(res.locals.userUrl, function(scripts) {
+			var inplace = [];
+			var sourceFiles = [];
+			var sourceFilesCrossDomain = [];
+			for(var i = 0; i < scripts.length; i++) {
+				var script = scripts[i];
+				switch (script.properties.type) {
+					case 'inplace':
+						inplace.push(script);
+						break;
+					case 'file_nonCrossDomain':
+						sourceFiles.push(script);
+						break;
+					case 'file_crossDomain':
+						sourceFilesCrossDomain.push(script);
+						break;
+				};
+			};
+			res.render('scripts', { all: scripts, 
+									inplace: inplace, 
+									sourceFiles: sourceFiles, 
+									sourceFilesCrossDomain: sourceFilesCrossDomain });
+		});
 };
