c9bb2bd01e2874bc5a0274356145260a7e4d08ff
Evgeni Ivakhnov <ivachnov@hotmail.com>
2013-02-03 18:59:53 +0100

    Save detected frameworks to the database

--- routes/extract.js
+++ routes/extract.js
@@ -17,19 +17,24 @@ module.exports = function(app){
     app.post('/extract', function(req, res){
     	var url = req.body.url;
     	// It is possible that the user gives a url without 'http://'
-		url = urlLib.addHttp(url);
+		urlHttp = urlLib.addHttp(url);
 		// Store the session for that user and use the url as identifier.
 		req.session.userUrl = url;
-    	extractor.parsePage(url, function(results) {
+    	extractor.parsePage(urlHttp, function(results) {
     		req.session.scriptsCount = results.scripts.length;
 			req.session.eventsCount = results.events.length;
     		
-    		console.log("Script tags: " + results.scripts.length);
-			console.log("DOM Events: " + results.events.length);
-			console.log("Frameworks: " + results.frameworks.length);
+
+			var scripts = results.scripts;
+			var events = results.events;
+			var frameworks = results.frameworks;
+
+    		console.log("Script tags: " + scripts.length);
+			console.log("DOM Events: " + events.length);
+			console.log("Frameworks: " + frameworks.length);
 			
 			//db.resetDb();	
-			db.savePage(url, results.scripts, results.events, function(err, reply) {
+			db.savePage(url, scripts, events, frameworks, function(err, reply) {
 				if(err) {
 					console.log('Catched an error in extractor.js');
 					console.log(err);
